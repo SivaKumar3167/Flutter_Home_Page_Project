bb3c287350f34f1fd36f8fc87fedfdbbaaf287ca
Sivakumar <sivakumar3167s@gmail.com>
2024-06-29 13:14:23 +0530

    First Commit

---
+++ lib/categories.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: non_constant_identifier_names
+
+final List<Map<String, Object>> Categories = [
+  {'title': 'Lottery', 'image': 'assets/images/ticket.png'},
+  {'title': 'Treasury', 'image': 'assets/images/star.png'},
+  {'title': 'Trivia', 'image': 'assets/images/question-mark.png'},
+  {'title': 'Karoke', 'image': 'assets/images/microphone.png'},
+  {'title': '#Hamilton', 'image': 'assets/images/H.png'},
+];
+
+final List<Map<String, Object>> News = [
+  {
+    'date': 'March 28',
+    'news': 'Hamilton Australia is back!',
+    'newsimage': 'assets/images/Hamilton.png'
+  },
+  {
+    'date': 'April 1',
+    'news': 'There is Nothing we can do',
+    'newsimage': 'assets/images/napolean.jpg'
+  },
+];
